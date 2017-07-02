741f15bd6fe1b0c7f185d6f1a32742e83b17b01f
harya <harya@LAPTOP-MV07580I>
2017-07-02 15:40:57 +0700

    New commit of newEmptyPHPWebPage.php

--- newEmptyPHPWebPage.php
+++ newEmptyPHPWebPage.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Netbeans, My Name Is Haryadi, Im noob on github."
+        echo "Netbeans, My Name Is Haryadi, Im noob on github.";
         ?>
     </body>
 </html>
